5a96efda3d305db750ffbff4c4cb35988dab03fe
Kendal <kendallbadilla10@gmail.com>
2026-03-13 10:51:15 -0600

    feat: eslint.config.js for environment configuration and linting

---
+++ eslint.config.js
@@ -0,0 +1,32 @@
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import prettierPlugin from 'eslint-plugin-prettier';
+
+export default [
+  {
+    // Configuración global
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 2022,
+        sourceType: 'module',
+      },
+      globals: {
+        NodeJS: 'readonly',
+        jest: 'readonly',
+      },
+    },
+  },
+  {
+    files: ['*.ts', '**/*.ts'],
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+      prettier: prettierPlugin,
+    },
+    rules: {
+      'prettier/prettier': 'error',
+      '@typescript-eslint/no-unused-vars': 'warn',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+    },
+  },
+];
