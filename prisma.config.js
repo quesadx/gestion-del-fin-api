ca6af8674fe5d7c4cfe5178b4adfed4bddaba426
Matteo Quesada <matteo.vargas.quesada@est.una.ac.cr>
2026-05-18 13:39:08 -0600

    feat(prisma): add prisma.config.js for database configuration

---
+++ prisma.config.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+
+module.exports = {
+  schema: 'prisma/schema.prisma',
+  migrations: {
+    path: 'prisma/migrations',
+    seed: 'tsx prisma/seed.ts',
+  },
+  datasource: {
+    url: process.env.DATABASE_URL,
+    shadowDatabaseUrl: process.env.SHADOW_DATABASE_URL,
+  },
+};
