091ef532db9c82b9900d20e86220a1dfe55380d4
Matteo Quesada <matteo.vargas.quesada@est.una.ac.cr>
2026-05-18 13:59:06 -0600

    fix(prisma.config.ts): reorder DATABASE_URL and DATABASE_DIRECT_URL for datasource configuration

--- prisma.config.ts
+++ prisma.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
     seed: 'tsx prisma/seed.ts',
   },
   datasource: {
-    url: process.env['DATABASE_DIRECT_URL'] ?? process.env['DATABASE_URL'],
+    url: process.env['DATABASE_URL'] ?? process.env['DATABASE_DIRECT_URL'],
     shadowDatabaseUrl: process.env['SHADOW_DATABASE_URL'],
   },
 });
